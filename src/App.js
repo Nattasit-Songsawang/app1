ce77a68d9109aecd8b61faf3e438f3b411dffd2b
Nattasit-Songsawang <s6703052411252@email.kmutnb.ac.th>
2025-12-25 08:45:51 +0700

    funtion component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import './style.css'
+import { Content, Footer, Header } from './func-components';
+
 
 function App() {
 return (
-  <>
-      <h2>DeveloperThai.com</h2>
-      <div className='title'>React & Native</div>
-  </>
+       [<Header/>,<Content/>,<Footer/>]
 )
   
 }
