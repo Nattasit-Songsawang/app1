91129d1694960b323a0cfcabef6ab651b1a1d561
Nattasit-Songsawang <s6703052411252@email.kmutnb.ac.th>
2025-12-25 07:35:41 +0700

    style
    style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import './style.css'
 
 function App() {
-  const divStyle={
-    color:'red',
-    backgroundColor:'powederblue',
-    fontSize:'larger',
-    padding:'3px'
-  }
-  return <div style={{color:'blue'}}>Hello React</div> 
+return (
+  <>
+      <h2>DeveloperThai.com</h2>
+      <div className='title'>React & Native</div>
+  </>
+)
   
 }
 
